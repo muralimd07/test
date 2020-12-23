0562e868e410b14b9c7d7fd91bcab79e6405191c
muralimd07 <60916429+muralimd07@users.noreply.github.com>
2020-12-23 02:02:52 -0800

    rebased

--- pgm.cs
+++ pgm.cs
@@ -10,11 +10,12 @@ public int mul(int a,int b)
 {
     return a*b;
 }
-public int div(int a,int b)
-{
-    return a-b;
-}
 public int mul(int a,int b)
 {
     return a,b;
 }
+public int div(int a,int b)
+{
+    return a-b;
+}
+
